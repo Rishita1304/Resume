c30a334339d73675e38df17617d033d15d6c2408
Rishita Srivastava <rishitasrivastava04@gmail.com>
2024-03-24 11:50:50 +0530

    skills added

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -18,7 +18,7 @@ const Skills = () => {
     "Vercel",
     "Render",
   ];
-  const db = ["MongoDB", "MySQL", "Firebase", "PostgreSQL"];
+  const db = ["MongoDB", "Firebase", "PostgreSQL"];
 
   return (
     <section className="mb-5 lg:mb-2.5">
