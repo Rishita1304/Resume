ec6d4a2ea86a31a548c5ff9a2bd340cba32920cf
Rishita Srivastava <rishitasrivastava04@gmail.com>
2024-05-03 20:21:08 +0530

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => {
             <div className="relative w-11/12 max-w-screen-lg p-8 m-4 leading-5 bg-white rounded-sm shadow-lg md:scale-100 scale print:m-0 print:p-0 print:w-auto print:shadow-none">
                 <button className='absolute p-2 text-xl text-gray-400 transition-all border rounded-full outline-none border-slate-400 top-6 right-6 hover:text-gray-800 hover:border-gray-800 print:hidden' onClick={() => { window.print() }}><AiFillPrinter /></button>
                 <Header />
-                {/* <About /> */}
+                <About />
                 <Skills />
                 <Experience />
                 <Projects />
