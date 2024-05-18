c7a23970a22baedf848bf57f5a08a7fad9ce623c
Rishita Srivastava <rishitasrivastava04@gmail.com>
2024-05-18 17:08:08 +0530

    project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -19,7 +19,7 @@ const Projects = () => {
         {
             title: "ImaginAI - AI-based SaaS platform",
             url: "https://imagin-ai-git-main-rishita1304s-projects.vercel.app/",
-            stack: "Next.js, OpenAI, Clerk, Stripe, MongoDB",
+            stack: "Next.js, Cloudinary AI, Clerk, Stripe, MongoDB",
             description: "An AI powered image proceesing platform that consolidates advanced image processing tools, secure payment integration, and robust search capabilities, empowering users to effortlessly enhance, manipulate, and manage visual content.",
         },
         {
