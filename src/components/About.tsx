7af54b40f88ba4ca5899b8cc765459c6e8ef12a9
Rishita Srivastava <rishitasrivastava04@gmail.com>
2024-11-12 11:48:50 +0530

    about changed

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,6 @@
 const About = () => {
     return (
-        <div className='mb-5 lg:mb-2.5'>A Full Stack Developer with expertise in MERN stack, Next.js, and React Native. Skilled in C++, JavaScript, and adept at delivering high-quality software solutions through strong problem-solving abilities.</div>
+        <div className='mb-5 lg:mb-2.5'>Full Stack Developer with expertise in MERN stack, Next.js, and React Native. Skilled in C++, JavaScript, and adept at delivering high-quality software solutions through strong problem-solving abilities.</div>
     );
 }
 
